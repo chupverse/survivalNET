737a23280dc2f8ab43eeb4b25968be7d6836dc70
Thomas Ollard <thomas.ollard@univ-poitiers.fr>
2025-03-12 10:18:19 +0100

    Update expectedhaz.R

--- R/expectedhaz.R
+++ R/expectedhaz.R
@@ -2,12 +2,20 @@
 # Expected instantaneous hazard, age in days
 # date in number of days since 1960 / age in years / time in days
 
-expectedhaz <- function(ratetable, age, year, sex, time) 
+expectedhaz <- function(ratetable, age, year, sex, time, max_age = NULL, max_year = NULL) 
 {
   time <- min(time, 1000000)
   .year <- as.numeric(format( as.Date(time + year, origin = "1960-01-01"), "%Y" ) ) 
   
-  ratetable[as.character( min( floor((age+time)/365.24), max(as.numeric(names(ratetable[, "2000", "male"]))) ) ),
-            as.character( min( .year, max(as.numeric(names(ratetable["51", , "male"]))) ) ),
+  if(is.null(max_age)){
+  max_age <- max(as.numeric(dimnames(ratetable)[[1]]))
+  }
+  
+  if(is.null(max_year)){
+  max_year <- max(as.numeric(dimnames(ratetable)[[2]]))
+  }
+  
+  ratetable[as.character( min( floor((age+time)/365.24), max_age ) ),
+            as.character( min( .year, max_year ) ),
             sex]
 }
